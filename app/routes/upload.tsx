a7cb9cdebc6c554afb903c041b70030aa4297f5c
sahilmane69 <mane50205@gmail.com>
2025-07-27 18:54:43 +0530

    Resume Feedback Page

--- app/routes/upload.tsx
+++ app/routes/upload.tsx
@@ -63,6 +63,7 @@ const upload = () => {
           await kv.set(`resume:${uuid}`, JSON.stringify(data));
           setStatusText('Analysis Complete, redirecting..')
           console.log(data);
+          navigate(`/resume/${uuid}`);
      }
      const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
           e.preventDefault();
